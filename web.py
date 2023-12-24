5d2cbe9aefe941b39051f1ec3e99c53a0d87a640
eric <eric123602@gmail.com>
2023-12-24 11:55:15 +0800

    fix:load template method

--- web.py
+++ web.py
@@ -16,77 +16,18 @@ def init_llm(temperature):
 
 # Map function for LLM chain
 def map_function(llm):
-    map_template = """您是專業的會議總結助理。您唯一的任務是為給定的會議記錄提供簡潔的摘要。確保最終總結和行動項目被翻譯成繁體中文，並且不包含任何個人意見或解釋。
-                    以下是會議內容逐字稿參考以上輸出格式來總結會議內容逐字稿，切記總結的內容要真實不可幻想，尤其是行動項目的部分
-                    '{docs}'
-                    """
+    with open("MMSummary/map_template.txt", "r") as f:
+        map_template = f.read()
     map_prompt = PromptTemplate.from_template(map_template)
     return LLMChain(llm=llm, prompt=map_prompt)
 
 # Reduce function for LLM chain
 def reduce_function(llm):
-    reduce_template ="""您是專業的會議總結助理。您唯一的任務是為給定的會議記錄提供簡潔的摘要。以下是一些規則：
-                        <規則>
-                        1.摘要應根據不同的主題分為不同的部分，每個部分都應以要點的形式來回答。
-                        2. 如果適用，您還應該列出行動項目，包括任務所有者和截止日期。
-                        3. 確保最終總結和行動項目被翻譯成繁體中文，並且不包含任何個人意見或解釋。
-
-                        這是輸出的參考的格式:
-                        ## 主題1
-
-                        - point 1
-                        - point 2
-                        ...
-                        - point N
-
-                        ## 主題2
-
-                        - point 1
-                        - point 2
-                        ...
-                        - point N
-
-                        ## 主題3
-                        - point 1
-                        - point 2
-                        ...
-                        - point N
-                        .
-                        .
-                        .
-                        ## 主題N
-                        - point 1
-                        - point 2
-                        ...
-                        - point N
-
-                        ## 行動項目
-
-                        - 負責人1: 任務名 
-                        - 負責人2: 任務名 
-                        ...
-                        - 負責人N: 任務名 
-
-                        以下是會議內容參考以上輸出格式來總結會議內容，切記總結的內容要真實不可幻想，尤其是行動項目的部分
-
-                        {docs}
-                        """
+    with open("MMSummary/reduce_template.txt", "r") as f:
+        reduce_template = f.read()
     reduce_prompt = PromptTemplate.from_template(reduce_template)
     return LLMChain(llm=llm, prompt=reduce_prompt)
 
-def map_reduce_function(token_max, map_chain, reduce_chain):
-    combine_documents_chain = StuffDocumentsChain(llm_chain=reduce_chain, document_variable_name="docs")
-    reduce_documents_chain = ReduceDocumentsChain(
-        combine_documents_chain=combine_documents_chain,
-        collapse_documents_chain=combine_documents_chain,
-        token_max=token_max,
-    )
-    return MapReduceDocumentsChain(
-        llm_chain=map_chain,
-        reduce_documents_chain=reduce_documents_chain,
-        document_variable_name="docs",
-        return_intermediate_steps=False,
-    )
 
 def process_map_results(file_content, llm, chunk_size,chunk_overlap):
     text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
@@ -99,32 +40,25 @@ def process_map_results(file_content, llm, chunk_size,chunk_overlap):
 def process_file(uploaded_file, chunk_size_1, chunk_overlap_1, chunk_size_2, chunk_overlap_2, temperature, token_max):
     file_name = uploaded_file.name.split(".")[0]
     file_content = uploaded_file.getvalue().decode("utf-8")
-    # text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    # file_content=text_splitter.create_documents([file_content])
-    # split_docs = text_splitter.split_documents(file_content)
 
     llm = init_llm(temperature)
 
-    # 第一階段映射
+    # First map stage
     first_map_results = process_map_results(file_content,llm, chunk_size_1, chunk_overlap_1)
     st.text_area("Map 1:",first_map_results[0], height=200)
-    # 第二階段映射
+    # Second map stage
     second_map_results = process_map_results(file_content,llm, chunk_size_2, chunk_overlap_2)
     st.text_area("Map 2:",second_map_results[0], height=200)
-    # 組合映射結果
+    # Merge map results
     combined_map_results = first_map_results + second_map_results
 
-    # 縮減階段
+    # Reduce stage
     reduce_chain = reduce_function(llm)
 
     combine_documents_chain = StuffDocumentsChain(llm_chain=reduce_chain, document_variable_name="docs")
     reduce_documents_chain = ReduceDocumentsChain(combine_documents_chain=combine_documents_chain,collapse_documents_chain=combine_documents_chain,token_max=token_max,)
     documents = [Document(page_content=content) for content in combined_map_results]
-    # combined_result, extra_info = reduce_documents_chain.combine_docs(documents)
     response = reduce_documents_chain.run(documents)
-    # reduce_chain = reduce_function(llm)
-    # map_reduce_chain = map_reduce_function(token_max, map_chain, reduce_chain)
-    # response = map_reduce_chain.run(split_docs)
     return response, file_name
 
 # Main function to run the Streamlit app
@@ -150,7 +84,7 @@ def main():
                 start = time.time()
                 st.session_state['response'], st.session_state['file_name'] = process_file(uploaded_file, chunk_size_1, chunk_overlap_1, chunk_size_2, chunk_overlap_2, temperature, token_max)
                 end=time.time()
-                st.success(f"處理完成!{round(end-start)}")
+                st.success(f"處理完成!耗時{round(end-start)}秒")
 
         if st.session_state['response']:
             st.text_area("Summary", st.session_state['response'], height=300)
