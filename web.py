e2da21b68277988150db4d7699aba2108f115b46
eric <eric123602@gmail.com>
2024-01-02 08:40:24 +0800

    feat: without use map.
    You can now choose whether to use map by checking it.

--- web.py
+++ web.py
@@ -11,16 +11,16 @@ from langchain_core.documents import Document
 import os
 import eval
 # Initialize the LLM chain
-def init_llm(temperature,model):
+def init_llm(temperature,model,max_tokens=1000):
     if model=="gpt-4-1106-preview":
-        return ChatOpenAI(temperature=temperature, model=model, api_key=os.environ['OPENAI_API_KEY'])
+        return ChatOpenAI(temperature=temperature, model=model, api_key=os.environ['OPENAI_API_KEY'],max_tokens=max_tokens)
     else:
-        return ChatOpenAI(openai_api_base="http://0.0.0.0:8000/v1",model="Taiwan-LLM-7B-v2.1-chat",temperature=0,api_key=os.environ['OPENAI_API_KEY'])
+        return ChatOpenAI(openai_api_base="http://0.0.0.0:8000/v1",model=model,temperature=0,api_key=os.environ['OPENAI_API_KEY'],max_tokens=max_tokens)
 
 
 # Map function for LLM chain
 def map_function(llm):
-    with open("./map_template.txt", "r") as f:
+    with open("./map_template2.txt", "r") as f:
         map_template = f.read()
     map_prompt = PromptTemplate.from_template(map_template)
     return LLMChain(llm=llm, prompt=map_prompt)
@@ -33,46 +33,69 @@ def reduce_function(llm):
     return LLMChain(llm=llm, prompt=reduce_prompt)
 
 # Process the map
-def process_map_results(split_docs, llm):
-    map_chain = map_function(llm)
-    return [map_chain.run(doc) for doc in split_docs]
+def process_map_results(split_docs):
+    temp=[]
+    start = time.time()
+    # map_chain = map_function(llm)
+    map_chain = map_function(init_llm(0,"gpt-4-1106-preview",1000))
+
+    st.write(f"共{len(split_docs)}個chunks")
+    bar = st.progress(0, text=" ")
+    for i, doc in enumerate(split_docs):
+        temp.append(map_chain.run(doc))
+        bar.progress((i+1)/len(split_docs)*0.99, text=f"完成第{i+1}個chunks 耗時{round(time.time()-start)}秒")
+    bar.empty()
+
+    return temp
 
 # Process the reduce
-def process_reduce_results(combined_map_results, llm,token_max):
-    reduce_chain = reduce_function(llm)
-    combine_documents_chain = StuffDocumentsChain(llm_chain=reduce_chain, document_variable_name="docs")
-    reduce_documents_chain = ReduceDocumentsChain(combine_documents_chain=combine_documents_chain,collapse_documents_chain=combine_documents_chain,token_max=token_max)
-    documents = [Document(page_content=content) for content in combined_map_results]
+def process_reduce_results(combined_map_results,token_max):
+    reduce_chain = reduce_function(init_llm(0,"gpt-4-1106-preview",4000))
+
+    prompt = PromptTemplate.from_template("折疊此內容: {docs}")
+    llm_chain = LLMChain(llm=init_llm(0,"gpt-4-1106-preview",4000), prompt=prompt)
+
+    collapse_documents_chain = StuffDocumentsChain(llm_chain=llm_chain,   document_separator="docs")
+    combine_documents_chain  = StuffDocumentsChain(llm_chain=reduce_chain, document_variable_name="docs")
+    reduce_documents_chain = ReduceDocumentsChain(combine_documents_chain=combine_documents_chain,collapse_documents_chain=collapse_documents_chain,token_max=token_max)
+    documents = [Document(page_content=str(content)) for content in combined_map_results]
     return reduce_documents_chain.run(documents)
 
 # Split the text into chunks
 def split_text(text, chunk_size, chunk_overlap):
-    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator=" ",
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+    )
     file_content=text_splitter.create_documents([text])
     split_docs = text_splitter.split_documents(file_content)
     return split_docs
 
 # Process the file
-def process_file(model,uploaded_file, chunk_size_1, chunk_overlap_1, chunk_size_2, chunk_overlap_2, temperature, token_max):
+def process_file(button,model,uploaded_file, chunk_size_1, chunk_overlap_1, chunk_size_2, chunk_overlap_2, temperature, token_max):
     file_name = uploaded_file.name.split(".")[0]
     file_content = uploaded_file.getvalue().decode("utf-8")
-
+    st.write(f"文件長度: {len(file_content)}")
     llm = init_llm(temperature,model)
 
     # First map stage
-    split_docs = split_text(file_content, chunk_size_1, chunk_overlap_1)
-    first_map_results = process_map_results(split_docs,llm)
-    st.text_area("Map 1:",first_map_results[0], height=200)
-
+    split_docs1 = split_text(file_content, chunk_size_1, chunk_overlap_1)
     # Second map stage
-    split_docs = split_text(file_content, chunk_size_2, chunk_overlap_2)
-    second_map_results = process_map_results(split_docs,llm)
-    st.text_area("Map 2:",second_map_results[0], height=200)
-    # Merge map results
-    combined_map_results = first_map_results + second_map_results
+    split_docs2 = split_text(file_content, chunk_size_2, chunk_overlap_2)
+    if button==False:
+        first_map_results = process_map_results(split_docs1)
+        st.text_area("Map 1:",first_map_results, height=200)
+
+        second_map_results = process_map_results(split_docs2)
+        st.text_area("Map 2:",second_map_results, height=200)
+        combined_map_results = first_map_results + second_map_results
+    else:
+        combined_map_results = split_docs1+split_docs2
+    st.write(len(combined_map_results))
 
     # Reduce stage
-    response=process_reduce_results(combined_map_results, llm,token_max)
+    response=process_reduce_results(combined_map_results,token_max)
 
     return response, file_name
 
@@ -82,16 +105,17 @@ def main():
     if 'response' not in st.session_state:
         st.session_state['response'] = None
         st.session_state['file_name'] = ""
-
+    
     tab1, tab2,tab3 = st.tabs(["Summary", "Settings","Evaluation"])
     with tab3:
         eval.main()
     with tab2:
-        model=st.selectbox("Choose model",["gpt-4-1106-preview","Taiwan-LLM-7B-v2.1-chat"])
-        chunk_size_1 = st.number_input("Chunk size 1", value=4000, min_value=1, max_value=10000, step=100)
-        chunk_overlap_1 = st.number_input("Chunk overlap 1", value=1000, min_value=0, max_value=10000, step=100)
-        chunk_size_2 = st.number_input("Chunk size 2", value=2000, min_value=1, max_value=10000, step=100)
-        chunk_overlap_2 = st.number_input("Chunk overlap 2", value=0, min_value=0, max_value=10000, step=100)
+        button=st.checkbox("不使用Map",value=False,help="不使用Map，將所有chunck合併直接使用Reduce")
+        model=st.selectbox("Choose model",["gpt-4-1106-preview","Taiwan-LLM-7B-v2.1-chat","openbuddy-deepseek-67b-v15.2"])
+        chunk_size_1 = st.number_input("Chunk size 1", value=16000, min_value=1, max_value=100000, step=100)
+        chunk_overlap_1 = st.number_input("Chunk overlap 1", value=4000, min_value=0, max_value=100000, step=100)
+        chunk_size_2 = st.number_input("Chunk size 2", value=8000, min_value=1, max_value=100000, step=100)
+        chunk_overlap_2 = st.number_input("Chunk overlap 2", value=0, min_value=0, max_value=100000, step=100)
         token_max = st.number_input("Token max", value=16000, min_value=1, max_value=100000, step=100, help="將Map生成的內容長度再次進行分割，避免超過LLM的長度限制")
         temperature = st.slider('Temperature', 0.0, 2.0, step=0.1)
 
@@ -100,7 +124,7 @@ def main():
         if uploaded_file is not None and (uploaded_file.name.split(".")[0] != st.session_state['file_name']):
             with st.spinner("處理中..."):
                 start = time.time()
-                st.session_state['response'], st.session_state['file_name'] = process_file(model,uploaded_file, chunk_size_1, chunk_overlap_1, chunk_size_2, chunk_overlap_2, temperature, token_max)
+                st.session_state['response'], st.session_state['file_name'] = process_file(button,model,uploaded_file, chunk_size_1, chunk_overlap_1, chunk_size_2, chunk_overlap_2, temperature, token_max)
                 end=time.time()
                 st.success(f"處理完成!耗時{round(end-start)}秒")
 
