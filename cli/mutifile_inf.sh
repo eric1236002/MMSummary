e5a242dd4e761a4f60c6c43eff95846c4faadfcc
eric <eric123602@gmail.com>
2024-01-11 21:40:01 +0800

    feat: use terminal to eval or inference

---
+++ cli/mutifile_inf.sh
@@ -0,0 +1,21 @@
+#執行cli.py 並且輸入參數  
+#參數1: 要執行的y 資料及名稱
+folder=$1
+
+#按照資料夾底下的檔案名稱執行
+for file in $folder/*
+do
+    echo $file
+    python3 cli.py \
+    --key "" \
+    --file $file \
+    --model "gpt-4-1106-preview" \
+    --chunk_size_1 16000 \
+    --chunk_overlap_1 0 \
+    --chunk_size_2 8000 \
+    --chunk_overlap_2 0 \
+    --token_max 16000 \
+    --temperature 0 \
+    --withoutmap false \
+    --output_dir "./cli/output"
+done
